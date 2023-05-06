b3fce1a532956d78c6fa31ba16deb9c436bdc710
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-05-06 10:48:59 -0500

    Rutas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://juanjoseporras2019:Lstqi7PA4jYCWyCb@recipes.dnqbtdf.mongodb.net/recipes?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+
+app.listen(3001, () => console.log("server started"));
---
+++ src/routes/recipes.js
@@ -0,0 +1,61 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipesModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await RecipesModel.find({});
+    res.json(response);
+  } catch (error) {
+    res.json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const recipe = new RecipesModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipesModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({savedRecipes: user.savedRecipes});
+  } catch (error) {
+    res.json(err);
+  }
+});
+
+router.get("/savedRecipes/ids", async (req, res) =>{
+    try {
+        const user = await UserModel.findById(req.body.userID);
+        res.json({savedRecipes: user?.savedRecipes});
+    } catch (err) {
+        res.json(err);
+    }
+} )
+
+router.get("/savedRecipes", async (req, res) =>{
+    try {
+        const user = await UserModel.findById(req.body.userID);
+        const savedRecipes = await RecipesModel.find({
+            _id: {$in: user.savedRecipes}
+        })
+        res.json({savedRecipes});
+    } catch (err) {
+        res.json(err);
+    }
+} )
+
+
+export { router as recipesRouter };
